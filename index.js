ffecd78d1a09958d3364e95b21bf385967de6c15
Menka shah <105760147+menkashah445@users.noreply.github.com>
2024-03-17 18:30:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const connectDB = require("./db");
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
 
+
+
 const app = express();
 const PORT = process.env.PORT || 8000;
 
@@ -23,11 +25,7 @@ app.use("/auth", authRoutes);
 // Define user routes
 app.use("/user", userRoutes);
 
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Something broke!");
-});
+// app.use("/forgotpassword", forgotPasswordRouter);
 
 // Start the server
 app.listen(PORT, () => {
